2bd5c974ae587e7066323ef466d75c9512ffe5ea
Dana He <danahe97@fb.com>
2017-07-20 15:10:54 -0700

    reimplemented location details

--- app/src/main/java/com/example/mbankole/tripplanner/fragments/LocationDetailFragment.java
+++ app/src/main/java/com/example/mbankole/tripplanner/fragments/LocationDetailFragment.java
@@ -1,8 +1,11 @@
 package com.example.mbankole.tripplanner.fragments;
 
-import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.mbankole.tripplanner.ExploreActivity;
@@ -28,11 +32,15 @@ import com.squareup.picasso.Picasso;
 public class LocationDetailFragment extends DialogFragment implements  View.OnClickListener{
     TextView tvName;
     TextView tvAddress;
+    TextView tvPhone;
+    TextView tvNowOpen;
+    TextView tvHours;
+    TextView tvprice;
     ImageView ivPhoto;
     Button btAdd;
     ImageButton btRemove;
 
-    ExploreActivity exploreActivity;
+    public ExploreActivity exploreActivity;
     PlanActivity planActivity;
 
     private final String TAG = "POIDETAILFRAGMENT";
@@ -58,14 +66,23 @@ public class LocationDetailFragment extends DialogFragment implements  View.OnCl
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Get field from view
+        final View viewref = view;
         final Location loc = getArguments().getParcelable("location");
         tvName = (TextView) view.findViewById(R.id.tvName);
         tvAddress = (TextView) view.findViewById(R.id.tvAddress);
+        tvHours = (TextView) view.findViewById(R.id.tvHours);
+        tvPhone = (TextView) view.findViewById(R.id.tvPhone);
+        tvNowOpen = (TextView) view.findViewById(R.id.tvNowOpen);
+        tvprice = (TextView) view.findViewById(R.id.tvPrice);
+        RatingBar ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
+        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+        stars.getDrawable(2).setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
         btAdd = (Button) view.findViewById(R.id.btAdd);
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 exploreActivity.addLocation(loc);
+                Snackbar.make(viewref, "Added!", Snackbar.LENGTH_SHORT).show();
             }
         });
         btRemove = (ImageButton) view.findViewById(R.id.ibRemove);
@@ -78,6 +95,25 @@ public class LocationDetailFragment extends DialogFragment implements  View.OnCl
         ivPhoto = (ImageView)view.findViewById(R.id.ivPhoto);
         tvName.setText(loc.name);
         tvAddress.setText(loc.address);
+        tvPhone.setText(loc.phoneNumber);
+        if (loc.openNow == null) {
+            tvNowOpen.setVisibility(View.GONE);
+        } else if (loc.openNow) {
+            tvNowOpen.setText("Open now");
+        } else {
+            tvNowOpen.setText("Now closed");
+        }
+        tvHours.setText(loc.hours);
+        String priceString = "";
+        for (int i = 0; i < loc.price; i++) {
+            priceString += "$";
+        }
+        tvprice.setText(priceString);
+        if (loc.rating != -1) {
+            ratingBar.setRating(loc.rating);
+        } else {
+            ratingBar.setVisibility(View.GONE);
+        }
         if (loc.photoUrl != null) {
             Picasso.with(getContext())
                     .load(loc.photoUrl)
